1a974fcba1b2cdbfddaee625b372bca892e5f6c6
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-02-12 07:52:20 +0530

    add list items from custom container

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyStore());
@@ -27,105 +28,37 @@ class MyListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var items = [
+      "Breakfast", "Dairy", "Vegetables", "Fruits", "Snacks", "Bread", "Desserts", "Home Care", "Personal Care", "Pet Care", "Kitchen"];
+    List<Widget> containers = [];
+
+    containers.addAll(createContainers(items));
+    containers.addAll(createContainers(items));
+    containers.addAll(createContainers(items));
+
     return ListView(
-        padding: const EdgeInsets.all(8),
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(20),
-              color: Colors.yellowAccent[100],
-              alignment: Alignment.center,
-            child: Text('Bread and Dairy',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.yellowAccent[200],
-            alignment: Alignment.center,
-            child: Text('Desserts',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.red[100],
-            alignment: Alignment.center,
-            child: Text('Fresh Fruits',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.red[200],
-            alignment: Alignment.center,
-            child: Text('Snacks',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.blue[100],
-            alignment: Alignment.center,
-            child: Text('Beverages',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.blue[200],
-            alignment: Alignment.center,
-            child: Text('Desserts',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.orange[100],
-            alignment: Alignment.center,
-            child: Text('Instant Foods',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.orange[200],
-            alignment: Alignment.center,
-            child: Text('Home Care',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.purple[100],
-            alignment: Alignment.center,
-            child: Text('Personal Care',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.purple[200],
-            alignment: Alignment.center,
-            child: Text('Pet Care',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold
-                )
-            ),
-          ),
-
-        ],
-
+      padding: const EdgeInsets.all(8),
+      children: containers,
     );
   }
 }
+
+List<Widget> createContainers(List<String> items) {
+  Random random = Random();
+  List<Widget> containers = [];
+  for (var item in items) {
+    containers.add(Container(
+      padding: const EdgeInsets.all(20),
+      color: Color.fromARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255)),
+  alignment: Alignment.center,
+  child:  Text(item,
+  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+    ));
+
+  }
+  return containers;
+}
+
 
 
 
